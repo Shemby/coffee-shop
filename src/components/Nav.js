26512d6dd0badb5336320d9a66704feead09339e
Sam <hembysam4@gmail.com>
2020-01-14 09:32:26 -0600

    7-1 architecture for sass

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 class Nav extends React.Component {
   render() {
     return (
-      <nav className='nav-bar'>
-        <NavLink className='nav-bar__item' to='/'>
-          Home
-        </NavLink>
+      <div className="nav-container">
+        <nav className="nav-bar">
+          <NavLink className="nav-bar__item" to="/">
+            Home
+          </NavLink>
 
-        <NavLink className='nav-bar__item' to='/About'>
-          About
-        </NavLink>
+          <NavLink className="nav-bar__item" to="/About">
+            About
+          </NavLink>
 
-        <NavLink className='nav-bar__item' to='/Contact'>
-          Contact
-        </NavLink>
+          <NavLink className="nav-bar__item" to="/Contact">
+            Contact
+          </NavLink>
 
-        <NavLink className='nav-bar__item' to='/Menu'>
-          Menu
-        </NavLink>
-      </nav>
+          <NavLink className="nav-bar__item" to="/Menu">
+            Menu
+          </NavLink>
+        </nav>
+      </div>
     );
   }
 }

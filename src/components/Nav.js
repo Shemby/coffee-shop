14e1f03f116eb5378b134f086ecc06a7356fdeba
Samuel Hemby <hembysam4@gmail.com>
2020-01-13 21:37:11 -0600

    working on navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,17 +1,28 @@
-import React from "react";
-import { NavLink, BrowserRouter } from "react-router-dom";
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-export default class Nav extends React.Component {
+class Nav extends React.Component {
   render() {
     return (
-      <nav className="nav-bar">
-        <BrowserRouter>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/About">About</NavLink>
-          <NavLink to="/Contact">Contact</NavLink>
-          <NavLink to="/Menu">Menu</NavLink>
-        </BrowserRouter>
+      <nav className='nav-bar'>
+        <NavLink className='nav-bar__item' to='/'>
+          Home
+        </NavLink>
+
+        <NavLink className='nav-bar__item' to='/About'>
+          About
+        </NavLink>
+
+        <NavLink className='nav-bar__item' to='/Contact'>
+          Contact
+        </NavLink>
+
+        <NavLink className='nav-bar__item' to='/Menu'>
+          Menu
+        </NavLink>
       </nav>
     );
   }
 }
+
+export default Nav;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter, Route } from "react-router-dom";
-import Landing from "./components/Landing";
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Menu from "./components/Menu";
-import Nav from "./components/Nav";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Landing from './components/Landing';
+import About from './components/About';
+import Contact from './components/Contact';
+import Menu from './components/Menu';
+import Nav from './components/Nav';
+import './scss/main.scss';
 
 class App extends React.Component {
   render() {
     return (
-      <React.Fragment>
+      <BrowserRouter>
         <Nav />
-        <BrowserRouter>
-          <Route path="/" exact component={Landing} />
-          <Route path="/Contact" component={Contact} />
-          <Route path="/About" component={About} />
-          <Route path="/Menu" component={Menu} />
-        </BrowserRouter>
-      </React.Fragment>
+        <Route path='/' exact component={Landing} />
+        <Route path='/Contact' component={Contact} />
+        <Route path='/About' component={About} />
+        <Route path='/Menu' component={Menu} />
+      </BrowserRouter>
     );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));

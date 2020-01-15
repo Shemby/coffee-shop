1de0f3fcba940cc7ac38f7319d8cfc1ba6549443
Sam <hembysam4@gmail.com>
2020-01-15 13:57:16 -0600

    rough menu complete

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -5,96 +5,164 @@ export default class Menu extends React.Component {
     return (
       <div className="menu">
         <h1 className="menu__heading">Menu</h1>
-        <div className="menu__columns">
+        <div className="categories">
           <div className="menu__category">
             <h3 className="menu__category--header">Coffee</h3>
-            <div className="menu__category--item">
-              <h4 className="menu__category--item-heading">Pour Over $2.50</h4>
-              <p className="menu__category--item-desc">
-                filter filled with our gourmet beans is placed over your mug and
-                then hot water is poured over resulting in a classic but more
-                refined flavor.
-              </p>
-            </div>
-            <div className="menu__category--item">
-              <h4 className="menu__category--item-heading">
-                French Press $3.00
-              </h4>
-              <p className="menu__category--item-desc">
-                Coffee grounds are added directly to hot water and then filtered
-                out using a press. Results in a much more robust flavor.
-              </p>
-            </div>
-            <div className="menu__category--item">
-              <h4 className="menu__category--item-heading">Cold Brew $3.00</h4>
-              <p className="menu__category--item-desc">
-                Extremely smooth cold coffee infused with nitrogen for a sweet
-                smooth flavor.
-              </p>
-            </div>
-            <div className="menu__category--item">
-              <div className="menu__category--item-heading">
-                <h4>Drip</h4>
-                <p>$2.00</p>
+            <div className="menu__category--item-container">
+              <div className="menu__category--item">
+                <div className="menu__category--item-heading">
+                  <h4>Pour Over</h4>
+                  <p>$2.00</p>
+                </div>
               </div>
-              <p className="menu__category--item-desc">
-                Coffee made the old fashioned way using our gourmet coffee.
-                grounds.
-              </p>
+              <div className="menu__category--item">
+                <div className="menu__category--item-heading">
+                  <h4>French Press</h4>
+                  <p>$2.50</p>
+                </div>
+              </div>
+              <div className="menu__category--item">
+                <div className="menu__category--item-heading">
+                  <h4>Nitro Cold Brew</h4>
+                  <p>$3.50</p>
+                </div>
+              </div>
+              <div className="menu__category--item">
+                <div className="menu__category--item-heading">
+                  <h4>Drip</h4>
+                  <p>$1.50</p>
+                </div>
+              </div>
             </div>
           </div>
           <div className="menu__category">
             <h3 className="menu__category--header">Espresso</h3>
-            <div className="menu__category--item">
-              <h4 className="menu__category--item-heading">Americano</h4>
-              <p className="menu__category--item-desc">
-                This is a description detailing the item in which the consumer
-                will purchase
-              </p>
-            </div>
-            <div className="menu__category--item">
-              <div className="menu__category--item-heading">
-                <h4>Latte</h4>
-                <p>$2.00</p>
+            <div className="menu__category--item-container">
+              <div className="menu__category--item">
+                <div className="menu__category--item-heading">
+                  <h4>Americano</h4>
+                  <p>$2.75</p>
+                </div>
               </div>
-              <p className="menu__category--item-desc">
-                Coffee made the old fashioned way using our gourmet coffee.
-                grounds.
-              </p>
-            </div>
-            <div className="menu__category--item">
-              <div className="menu__category--item-heading">
-                <h4>Macchiato</h4>
-                <p>$2.00</p>
+              <div className="menu__category--item">
+                <div className="menu__category--item-heading">
+                  <h4>Latte</h4>
+                  <p>$4.45</p>
+                </div>
               </div>
-              <p className="menu__category--item-desc">
-                Coffee made the old fashioned way using our gourmet coffee.
-                grounds.
-              </p>
-            </div>
-            <div className="menu__category--item">
-              <div className="menu__category--item-heading">
-                <h4>Fireside Mocha</h4>
-                <p>$2.00</p>
+              <div className="menu__category--item">
+                <div className="menu__category--item-heading">
+                  <h4>Macchiato</h4>
+                  <p>$4.45</p>
+                </div>
               </div>
-              <p className="menu__category--item-desc">
-                Coffee made the old fashioned way using our gourmet coffee.
-                grounds.
-              </p>
-            </div>
-            <div className="menu__category--item">
-              <div className="menu__category--item-heading">
-                <h4>White Chocolate Mocha</h4>
-                <p>$2.00</p>
+              <div className="menu__category--item">
+                <div className="menu__category--item-heading">
+                  <h4>Fireside Mocha</h4>
+                  <p>$4.25</p>
+                </div>
               </div>
-              <p className="menu__category--item-desc">
-                Coffee made the old fashioned way using our gourmet coffee.
-                grounds.
-              </p>
+              <div className="menu__category--item">
+                <div className="menu__category--item-heading">
+                  <h4>White Chocolate Mocha</h4>
+                  <p>$4.25</p>
+                </div>
+              </div>
+              <div className="menu__category--item">
+                <div className="menu__category--item-heading">
+                  <h4>Cappuccino</h4>
+                  <p>$3.25</p>
+                </div>
+              </div>
+              <div className="menu__category--item">
+                <div className="menu__category--item-heading">
+                  <h4>Red Eye</h4>
+                  <p>$2.00</p>
+                </div>
+              </div>
             </div>
           </div>
           <div className="menu__category">
             <h3 className="menu__category--header">Non-Coffee</h3>
+            <div className="menu__category--item-container">
+              <div className="menu__category--item">
+                <div className="menu__category--item-heading">
+                  <h4>Strawberry Bannana Smoothie</h4>
+                  <p>$3.50</p>
+                </div>
+              </div>
+              <div className="menu__category--item">
+                <div className="menu__category--item-heading">
+                  <h4>Mixed Berry Smoothie</h4>
+                  <p>$3.50</p>
+                </div>
+              </div>
+              <div className="menu__category--item">
+                <div className="menu__category--item-heading">
+                  <h4>Loose Leaf Tea</h4>
+                  <p>$1.50</p>
+                </div>
+              </div>
+              <div className="menu__category--item">
+                <div className="menu__category--item-heading">
+                  <h4>Iced Tea</h4>
+                  <p>$1.50</p>
+                </div>
+              </div>
+              <div className="menu__category--item">
+                <div className="menu__category--item-heading">
+                  <h4>Chai Tea</h4>
+                  <p>$2.00</p>
+                </div>
+              </div>
+              <div className="menu__category--item">
+                <div className="menu__category--item-heading">
+                  <h4>Hot Chocolate</h4>
+                  <p>$2.50</p>
+                </div>
+              </div>
+              <div className="menu__category--item">
+                <div className="menu__category--item-heading">
+                  <h4>Chocolate/Vanilla Milkshake</h4>
+                  <p>$3.50</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="menu__category">
+            <h3 className="menu__category--header">Pastries</h3>
+            <div className="menu__category--item-container">
+              <div className="menu__category--item">
+                <div className="menu__category--item-heading">
+                  <h4>Cheese Danish</h4>
+                  <p>$2.75</p>
+                </div>
+              </div>
+              <div className="menu__category--item">
+                <div className="menu__category--item-heading">
+                  <h4>Scone</h4>
+                  <p>$2.25</p>
+                </div>
+              </div>
+              <div className="menu__category--item">
+                <div className="menu__category--item-heading">
+                  <h4>Chocolate Chip Cookie</h4>
+                  <p>$1.50</p>
+                </div>
+              </div>
+              <div className="menu__category--item">
+                <div className="menu__category--item-heading">
+                  <h4>Oatmeal Cookie</h4>
+                  <p>$1.50</p>
+                </div>
+              </div>
+              <div className="menu__category--item">
+                <div className="menu__category--item-heading">
+                  <h4>Blueberry Muffin</h4>
+                  <p>$2.25</p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,25 +4,23 @@ import { NavLink } from "react-router-dom";
 class Nav extends React.Component {
   render() {
     return (
-      <div className="nav-container">
-        <nav className="nav-bar">
-          <NavLink className="nav-bar__item" to="/">
-            Home
-          </NavLink>
+      <nav className="nav-bar">
+        <NavLink className="nav-bar__item" to="/">
+          Home
+        </NavLink>
 
-          <NavLink className="nav-bar__item" to="/About">
-            About
-          </NavLink>
+        <NavLink className="nav-bar__item" to="/About">
+          About
+        </NavLink>
 
-          <NavLink className="nav-bar__item" to="/Contact">
-            Contact
-          </NavLink>
+        <NavLink className="nav-bar__item" to="/Contact">
+          Contact
+        </NavLink>
 
-          <NavLink className="nav-bar__item" to="/Menu">
-            Menu
-          </NavLink>
-        </nav>
-      </div>
+        <NavLink className="nav-bar__item" to="/Menu">
+          Menu
+        </NavLink>
+      </nav>
     );
   }
 }
